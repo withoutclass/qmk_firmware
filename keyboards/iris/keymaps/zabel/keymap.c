0ca6b0798f74825d1e8e61d24698c7ddb48c3fe0
Matt Langreder <withoutclass@gmail.com>
2019-04-08 14:58:52 -0500

    fix

--- keyboards/iris/keymaps/zabel/keymap.c
+++ keyboards/iris/keymaps/zabel/keymap.c
@@ -18,11 +18,12 @@ enum custom_keycodes {
 #define KC_XXXX KC_NO
 #define KC_DVK DVORAK
 #define KC_NAV NAVIGATION
-#define KC_SHENT MT(MOD_RSFT,KC_ENT)
+#define KC_SHBSLS MT(MOD_RSFT,KC_BSLS)
 #define KC_LBRALT MT(MOD_LALT, KC_LBRC)
 #define KC_RBRALT MT(MOD_RALT, KC_RBRC)
 #define KC_LGENT MT(MOD_LGUI, KC_ENT)
 #define KC_NSPC LT(_NAVIGATION, KC_SPC)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -32,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      TAB ,QUOT,COMM,DOT , P  , Y  ,                F  , G  , C  , R  , L  ,SLSH,
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     LCTL, A  , O  , E  , U  , I  ,                D  , H  , T  , N  , S  ,MINS,
+     LCTL,  A ,  O  , E  , U  , I  ,                D  , H  , T  , N  , S  ,MINS,
   //,----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----.
-     LSFT,SCLN, Q  , J  , K  , X  ,EQL,     GRAVE , B  , M  , W  , V  , Z  ,SHENT,
+     LSFT,SCLN, Q  , J  , K  , X  ,EQL,     GRAVE , B  , M  , W  , V  , Z  ,SHBSLS,
   //,----+----+----+----+----+----+----/    \----+----+----+----+----+----+----.
-                      LBRC,LBRALT,LGENT,     NSPC,RBRALT,RBRC
+                      LCTL,LBRALT,LGENT,     NSPC,RBRALT,CAPS
   //                  `----+----+----'       `----+----+----'
   ),
 
@@ -44,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
       F1 , F2 , F3 , F4 , F5 , F6 ,                F7 , F8 , F9 ,F10 ,F11 ,F12 ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-       ,     ,      ,VOLU,MPLY,    ,                AT ,PGUP, UP ,PGDN,HOME,END ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,    ,    ,VOLU,MPLY,    ,                AT ,PGUP, UP ,PGDN,HOME,END ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|œ
          ,   , MRWD,VOLD,MFFD,    ,               BSLS,LEFT,DOWN,RGHT,INS ,DEL ,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
          ,    ,    ,    ,     ,    ,   ,         ,CIRC,PSCR,SLCK,PAUS,APP ,PIPE,
